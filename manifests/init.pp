1d307647fc12e7c28f714efc46954feb9c5af8e5
Zavisa Nikac <znikac@srce.hr>
2016-12-12 14:44:52 +0100

    version 8 upgrade

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,7 @@
 class cuda (
   $version55 = true,
   $version7 = true,
+  $version8 = true,
 ) {
   require yum::repo::cuda
 
@@ -38,6 +39,24 @@ class cuda (
       group   => 'root',
       mode    => '0644',
       source  => 'puppet:///modules/cuda/cuda-7-0',
+    }
+  }
+  if $version8 {
+    package { 'cuda-core-8-0':
+      ensure  => present,
+    }
+    package { 'cuda-runtime-8-0':
+      ensure  => present,
+    }
+    # dev packages
+    package { [ 'cuda-cudart-dev-8-0', 'cuda-cublas-dev-8-0', 'cuda-cufft-dev-8-0', 'cuda-curand-dev-8-0', 'cuda-cusolver-dev-8-0', 'cuda-cusparse-dev-8-0', 'cuda-npp-dev-8-0', 'cuda-nvrtc-dev-8-0' ]:
+      ensure  => present,
+    }
+    file { '/etc/modulefiles/cuda-8-0':
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      source  => 'puppet:///modules/cuda/cuda-8-0',
     }
   }
 }
